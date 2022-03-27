cf192933db303559320a5c8282455ac3cbbc6061
Nichola92 <muthuarachchim@student.douglascollege.ca>
2022-03-26 21:03:41 -0700

    Updated code for AddBook class

--- app/src/main/java/com/example/groupproject/AddingBook.java
+++ app/src/main/java/com/example/groupproject/AddingBook.java
@@ -2,7 +2,15 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddingBook extends AppCompatActivity {
 
@@ -10,5 +18,65 @@ public class AddingBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adding_book);
+
+        EditText txtBookTitle = (EditText) findViewById(R.id.txtBookTitle);
+        EditText txtAuthorName = (EditText) findViewById(R.id.txtAuthor);
+        EditText txtPubisherName = (EditText) findViewById(R.id.txtPublisherName);
+        EditText txtPublicationYear =(EditText) findViewById(R.id.txtPublicationYear);
+        CheckBox chkShare = (CheckBox) findViewById(R.id.chkShare);
+        CheckBox chkRent = (CheckBox) findViewById(R.id.chkRent);
+        CheckBox chkGiveAway = (CheckBox) findViewById(R.id.chkGiveaway);
+        Button btnAddBook = (Button) findViewById(R.id.btnAddUpdate);
+        Button btnCancel = (Button) findViewById(R.id.btnCancelAdd);
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        int userid = sharedPreferences.getInt("key1",0);
+
+        DBHelper  dbHelper= new DBHelper(this);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddingBook.this, Home.class));
+            }
+        });
+
+        btnAddBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String bookTitle = txtBookTitle.getText().toString();
+                String author = txtAuthorName.getText().toString();
+                String publisher = txtPubisherName.getText().toString();
+                String publicationYear = txtPublicationYear.getText().toString();
+                String borrowActivity = "";
+
+                if(chkShare.isChecked())
+                {
+                    borrowActivity = "Share";
+                }
+                else if(chkRent.isChecked())
+                {
+                    borrowActivity = "Rent";
+                }
+                else if(chkGiveAway.isChecked())
+                {
+                    borrowActivity = "GiveAway";
+                }
+
+                Boolean insert = dbHelper.insertBookData(bookTitle,author,publisher,publicationYear,borrowActivity,1);
+                if(insert){
+                    Toast.makeText(AddingBook.this,"Book is Successfully Added", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(AddingBook.this, Home.class));
+                }
+                else{
+
+                    Toast.makeText(AddingBook.this,"Book is not added. Please try again.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(AddingBook.this, Home.class));
+                }
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/groupproject/DBHelper.java
+++ app/src/main/java/com/example/groupproject/DBHelper.java
@@ -16,6 +16,7 @@ public class DBHelper extends SQLiteOpenHelper{
     public static final String DBNAME = "BookSharing.db";
     final static int DATABASE_VERSION = 1;
     final static String TABLE1_NAME = "Users_Information";
+    final static String TABLE2_NAME = "Book_Information";
     final static String T1COL1 = "Id";
     final static String T1COL2 = "Username";
     final static String T1COL3 = "Email";
@@ -23,6 +24,15 @@ public class DBHelper extends SQLiteOpenHelper{
     final static String T1COL5 = "Age";
     final static String T1COL6 = "Interest";
     final static String T1COL7 = "Password";
+    final static String T2COL1 = "BookID";
+    final static String T2COL2 = "BookTitle";
+    final static String T2COL3 = "AuthorName";
+    final static String T2COL4 = "PublisherName";
+    final static String T2COL5 = "PublicationYear";
+    final static String T2COL6 = "BorrowActivity";
+
+
+
 
 
     public DBHelper( Context context)  {
@@ -34,16 +44,28 @@ public class DBHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase MyDatabase) {
-        String query = "CREATE TABLE " + TABLE1_NAME + "(" + T1COL1 + " Integer PRIMARY KEY,"+
+        String query1 = "CREATE TABLE " + TABLE1_NAME + "(" + T1COL1 + " Integer PRIMARY KEY,"+
                 T1COL2 + " Text, " + T1COL3 + " Text, " + T1COL4 + " Text, " +
                 T1COL5 + " Text, " + T1COL6 + " Text, " + T1COL7 + " Text)";
 
-        MyDatabase.execSQL(query);
+        MyDatabase.execSQL(query1);
+
+        String query2 ="CREATE TABLE " +
+                TABLE2_NAME +
+                "(" + T2COL1 + " Integer PRIMARY KEY,"+
+                T2COL2 + " Text, " +
+                T2COL3 + " Text, " +
+                T2COL4 + " Text, " +
+                T2COL5 + " Integer," +
+                "FOREIGN KEY(" + T1COL1 + ") REFERENCES " +TABLE1_NAME+"("+T1COL1 + "))";
+
+        MyDatabase.execSQL(query2);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase MyDatabase, int oldVersion, int newVersion) {
         MyDatabase.execSQL("DROP TABLE " + TABLE1_NAME);
+        MyDatabase.execSQL("DROP TABLE " + TABLE2_NAME);
         onCreate(MyDatabase);
     }
 
@@ -65,6 +87,28 @@ public class DBHelper extends SQLiteOpenHelper{
             return true;
         }
 
+    }
+
+    public Boolean insertBookData(String bookTitle, String authorName, String publisherName, String publicationYear,
+                                  String borrowActivity, int userId){
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(T2COL2,bookTitle);
+        contentValues.put(T2COL3,authorName);
+        contentValues.put(T2COL4,publisherName);
+        contentValues.put(T2COL5,publicationYear);
+        contentValues.put(T2COL6,borrowActivity);
+        contentValues.put(T1COL1,userId);
+
+        long result = MyDatabase.insert(TABLE2_NAME,null,contentValues);
+
+        if(result==-1){
+            return false;
+        }
+        else{
+
+            return true;
+        }
     }
 
     public Boolean checkusername(String username){
